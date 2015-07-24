ad298b1bcc3a764b5b94723e7a2c12f2850577fc
Andrew Breckenridge <asbreckenridge@me.com>
2015-07-23 20:20:34 -0700

    init

---
+++ listen-in/listen-in/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  listen-in
+//
+//  Created by Andrew Breckenridge on 7/23/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ listen-in/listen-in/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  listen-in
+//
+//  Created by Andrew Breckenridge on 7/23/15.
+//  Copyright (c) 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
